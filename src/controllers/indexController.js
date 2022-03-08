e35575be09694dd9bde4ff04c3c7db9bfb94e426
raju-gurubrahma <gurubrahmac@gmail.com>
2022-03-08 15:49:01 +0530

    logs the user details

--- src/controllers/indexController.js
+++ src/controllers/indexController.js
@@ -1,8 +1,19 @@
 /* eslint-disable prettier/prettier */
 const comments = require('./commentsController')
 
-module.exports = async function(repo, isoString) {
+module.exports = async function (repo, isoString) {
   // Fetch Comments
   const users = await comments(repo, isoString)
-  console.log(users)
+  // Print Benchmarks & Report
+  // Log empty lines for readability
+  console.log('')
+  console.log('')
+
+  users.map((user) => {
+    let comments = user.comments.toString()
+    let login = user.login
+    let commits = user.total
+
+    console.log(`${comments}  comments, ${login}  (${commits} commits)`)
+  })
 }
